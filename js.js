b4a5e03f3f44214f56dbaf17d791a7b9235cce74
AngelaMallet <angelamallet@gmail.com>
2018-10-11 11:59:19 +0200

    ex2

---
+++ js.js
@@ -0,0 +1,8 @@
+function show(){
+var text = document.getElementById('text')
+text.style.display = 'block';
+}
+function hide(){
+var text= document.getElementById('text')
+text.style.display = 'none';
+}
